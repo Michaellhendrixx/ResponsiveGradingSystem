a1281c3117162d40e271b48bc3461f2a4a8834e2
Michaellhendrixx <160848834+Michaellhendrixx@users.noreply.github.com>
2024-04-28 11:56:51 +0100

    Add files via upload

---
+++ responsiveGradingSystem.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+no=int(input('Enter your score'))
+
+
+# In[ ]:
+
+
+if no<=79 and no>=75:
+    print('you have A grade')
+
+
+# In[ ]:
+
+
+if no<=75 and no>=70:
+    print('you have A- grade')
+
+
+# In[ ]:
+
+
+if no<=70 and no>=65:
+    print('you have B+ grade')
+
+
+# In[ ]:
+
+
+if no<=65 and no>=60:
+    print('you have B grade')
+
+
+# In[ ]:
+
+
+if no<=60 and no>=50:
+    print('you have C+ grade')
+
+
+# In[ ]:
+
+
+if no<=50 and no>=40:
+    print('you have C grade')
+
+
+# In[ ]:
+
+
+if no<0:
+   print('invalid option')
+
+
+# In[ ]:
+
+
+else no<=39:
+    print('sorry you fail, better up next time')
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
